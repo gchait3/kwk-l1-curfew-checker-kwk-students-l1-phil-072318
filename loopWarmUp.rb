703d2033ba1cfc9f800e6017810062556601820b
Grace T <gtsai64@gmail.com>
2018-07-25 13:08:03 +0000

    automatically backed up by learn

--- loopWarmUp.rb
+++ loopWarmUp.rb
@@ -5,9 +5,5 @@ def Workout
     until x = 4
       puts "Hammer Curl Right"
       puts "Hammer Curl Left"
-    until x = 8
-      puts "Jump Squat"
-    until x = 16
-      puts "Sit Up"
   end
 end
